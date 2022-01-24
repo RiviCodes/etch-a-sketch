113c165a8022324e92da2094b06400136eb0cf6a
RiviCodes <jose.riveramz@udlap.mx>
2022-01-24 12:15:29 -0600

    Add the 'resetGrid' function

--- script.js
+++ script.js
@@ -18,16 +18,28 @@ function drawGrid(userGrid = defaultGrid) {
   }
 }
 
+// Add color to the grid's squares/boxes when hovering
 gridContainer.addEventListener('mouseover', getColor);
 
 function getColor(event) {
   let square = event.target;
   return square.style.backgroundColor = 'black';
-  // Multi-color test
+  // RGB test
   /*
   let red = Math.floor((Math.random() * 256) + 1);
   let green = Math.floor((Math.random() * 256) + 1);
   let blue = Math.floor((Math.random() * 256) + 1);
   return square.style.backgroundColor = `rgb(${red},${green},${blue})`;
   */
+}
+
+// Resets the grid & creates a new one
+function resetGrid() {
+  let userGrid = prompt('What grid\'s size do you want? (i.e 16x16)\nType a single value! (i.e 16)\n(Avoid using numbers higher than 100)');
+  if (userGrid <= 100) {
+    let gridSize = userGrid;
+    drawGrid(gridSize);
+  } else {
+    alert('ERROR\nGrid size must be less than and/or equal to 100')
+  }
 }
